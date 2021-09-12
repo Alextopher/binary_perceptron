7b30d14b09d92f8fd2415819b29878119f0213d6
mahonec <mahonec@clarkson.edu>
2021-09-12 15:01:03 -0400

    read data from file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import numpy as np
+
+pairs = []
+with open('train-a1-449.txt') as f:
+    for line in f.readlines():
+        split = line.split(' ')
+        nums = np.array([float(n) for n in split[:1024]])
+        is_yes = split[-1] == 'Y'
+        pairs.append((nums, is_yes))
