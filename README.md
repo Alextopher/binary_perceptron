f00c473b16928ecaeff41821f355b08a1304b88e
mahonec <mahonec@clarkson.edu>
2021-09-23 09:14:27 -0400

    finished assignment

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # binary_perceptron
-Binary Perceptron written for CS449 and CS549 at Clarkson University
+Binary Perceptron written for CS449 at Clarkson University. The uses the simple perceptron algorithm that can solves problems where there is a "perfect" solution.
+
+## Run
+
+`python3 main.py`
+
+## Dependencies 
+
+This was written using `Python 3.9.7` and `numpy 1.20.1`. It should work in most scenarios.
+
+## Format
+
+Each point in the train set is represented as space-seperated floats followed by a `Y` or `N` and then a `\n` newline.
+
+For example in the 3d case:
+
+```
+-187.52495221 1226.75921175 1817.26741526 Y
+1012.31895965 5.14374519193 337.286686965 N
+```
--- main.py
+++ main.py
@@ -1,9 +1,68 @@
-import numpy as np
+#!python3
 
+import numpy as np
+import sys
+
+# Number of dimensions
+N = 1024
+
+# read data from file
 pairs = []
-with open('train-a1-449.txt') as f:
+with open(sys.argv[1]) as f:
     for line in f.readlines():
         split = line.split(' ')
-        nums = np.array([float(n) for n in split[:1024]])
-        is_yes = split[-1] == 'Y'
-        pairs.append((nums, is_yes))
+        
+        # convert string numbers to floats
+        v = np.array([float(n) for n in split[:N]])
+
+        # normalize inputs vectors to have magnitude 1
+        # this doesn't change what side of the plane the points lie on
+        # as long as our peceptron is going through the origin
+        v = v / np.linalg.norm(v)
+
+        # extract the boolean 
+        is_yes = split[-2] == 'Y'
+
+        pairs.append((v, is_yes))
+
+
+## Simple binary perceptron algorithm
+# error function
+def error(weights, pairs):
+    return sum([(np.dot(weights, x) >= 0) == b for (x, b) in pairs]) / float(len(pairs))
+
+# Weights all start at 0
+weights = np.zeros(N)
+t = 0
+time_since_error = 0
+
+print("maximum interations: " + str(len(pairs) * 20))
+for (x, y) in pairs * 20:
+    # w * x > 0
+    prediction = np.dot(weights, x) >= 0
+    t += 1
+
+    # if we guessed right there is no change to make
+    if prediction != y:
+        # if the correct answer is positive or negative
+        if y:
+            weights += x
+        else:
+            weights -= x
+
+        time_since_error = 0
+    else:
+        time_since_error += 1
+
+    # check if we've correctly predicted every training point
+    if time_since_error == len(pairs):
+        break
+
+# Save weights in Tino's format
+outfile = sys.argv[2] if len(sys.argv) >= 3 else 'weights.txt'
+with open(outfile, 'w') as f:
+    f.write(" ".join([str(w) for w in weights]))
+
+print("wrote weight to " + outfile)
+print("correctness: " + str(error(weights, pairs) * 100) + "%")
+print("iterations (how many dot products): " + str(t))
